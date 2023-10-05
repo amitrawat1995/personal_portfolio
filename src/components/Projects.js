08f0a10363edb7e4fc5c9e2c8e6e42b3f9cc77bd
amitrawat <amitrawat037@gmail.com>
2023-10-05 23:14:13 +0530

    ok

---
+++ src/components/Projects.js
@@ -0,0 +1,65 @@
+import { Container, Row, Col} from "react-bootstrap"
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    }
+  ] 
+
+  return(
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            hey
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+
+    )
+}
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
